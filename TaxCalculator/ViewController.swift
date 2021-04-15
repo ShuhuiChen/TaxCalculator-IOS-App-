fa12ff8a053be4063579a74e4af582c03a8623d7
Sharon Chen <36015822+ShuhuiChen@users.noreply.github.com>
2021-04-15 19:47:08 +0800

    Create ViewController.swift

---
+++ TaxCalculator/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  TexCalculator
+//
+//  Created by shuhui chen on 2021-04-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	
+	@IBOutlet weak var totalPriceLbl: UILabel!
+	@IBOutlet weak var PriceTxt: UITextField!
+	@IBOutlet weak var SalesTxt: UITextField!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+		totalPriceLbl.text = ""
+	}
+
+
+	@IBAction func CalculatePrice(_ sender: Any) {
+		let price = Double(PriceTxt.text!)!
+		let tex = Double(SalesTxt.text!)!
+		
+		let totalPrice = price * tex + price
+		totalPriceLbl.text = "$\(totalPrice)"
+	}
+}
+
